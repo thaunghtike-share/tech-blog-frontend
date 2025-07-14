4108542ab347cdcdba1bd4c801dae30e091abd90
thaunghtike-share <zwe.zzzin@gmail.com>
2025-07-14 17:15:30 +0630

    add read count

--- app/free-labs/page.tsx
+++ app/free-labs/page.tsx
@@ -13,8 +13,16 @@ export default function LearnDevOpsOnUtubePage() {
   const searchQuery = searchParams.get("search") || ""
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-indigo-50 via-purple-50 to-pink-50 relative overflow-x-hidden">
-    {/* Messenger Support Floating Button */}
+    <div className="min-h-screen bg-gray-50 relative overflow-x-hidden">
+      {/* Subtle background pattern */}
+      <div
+        className="absolute inset-0 z-0 opacity-10"
+        style={{
+          backgroundImage:
+            "url(\"data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fillRule='evenodd'%3E%3Cg fill='%239C92AC' fillOpacity='0.1'%3E%3Cpath d='M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0 0v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zM12 34v-4h-2v4H6v2h4v4h2v-4h4v-2h-4zm0 0v-4h-2v4H6v2h4v4h2v-4h4v-2h-4zM36 10v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0 0v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zM12 10v-4h-2v4H6v2h4v4h2v-4h4v-2h-4zm0 0v-4h-2v4H6v2h4v4h2v-4h4v-2h-4z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E\")",
+        }}
+      ></div>
+      {/* Messenger Support Floating Button */}
       <a
         href="https://m.me/learndevopsnowbytho"
         target="_blank"
@@ -29,7 +37,14 @@ export default function LearnDevOpsOnUtubePage() {
           className="w-10 h-10 rounded-full"
         >
           <defs>
-            <linearGradient id="messengerGradient" x1="0" y1="0" x2="240" y2="240" gradientUnits="userSpaceOnUse">
+            <linearGradient
+              id="messengerGradient"
+              x1="0"
+              y1="0"
+              x2="240"
+              y2="240"
+              gradientUnits="userSpaceOnUse"
+            >
               <stop stopColor="#E1306C" />
               <stop offset="1" stopColor="#833AB4" />
             </linearGradient>
@@ -49,5 +64,5 @@ export default function LearnDevOpsOnUtubePage() {
       <FreeLabs />
       <MinimalFooter />
     </div>
-  )
+  );
 }
