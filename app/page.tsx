5bbc2b8a5457b40190e207260953d9f320fa64d2
thaunghtike-share <zwe.zzzin@gmail.com>
2025-07-23 14:09:07 +0630

    add career path

--- app/page.tsx
+++ app/page.tsx
@@ -94,7 +94,7 @@ export default function HomePage() {
         <div className="-mt-40 md:-mt-38">
           <RecommendedPaidCourses />
         </div>
-        <div className="-mt-22">
+        <div className="-mt-31 md:-mt-26">
           <FreelanceServicesSection />
         </div>
         <div className="-mt-24">
