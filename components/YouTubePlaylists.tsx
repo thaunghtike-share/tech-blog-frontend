315b4be70404ebe6fd76bb2c7006329b3537570d
thaunghtike-share <zwe.zzzin@gmail.com>
2025-07-23 10:47:29 +0630

    add career path

--- components/YouTubePlaylists.tsx
+++ components/YouTubePlaylists.tsx
@@ -281,7 +281,7 @@ export function YouTubePlaylists({
                     animate={{ opacity: 1, y: 0 }}
                     transition={{ delay: idx * 0.08 }}
                     whileHover={{ y: -8, scale: 1.02 }}
-                    className={`flex-shrink-0 w-[85vw] snap-center sm:w-auto group bg-white rounded-xl shadow-lg border-l-4 ${config.border} overflow-hidden transition-all duration-300 hover:shadow-xl flex flex-col`}
+                    className={`flex-shrink-0 w-[75vw] snap-center sm:w-auto group bg-white rounded-xl shadow-lg border-l-4 ${config.border} overflow-hidden transition-all duration-300 hover:shadow-xl flex flex-col`}
                   >
                     <div className="relative aspect-video bg-gray-900 overflow-hidden">
                       <iframe
