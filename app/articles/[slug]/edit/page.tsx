0cd483fbef0750a5c772e08e3168ad4f290cb9ee
thaunghtike-share <zwe.zzzin@gmail.com>
2025-07-31 11:49:40 +0630

    polish edit page full screen to write article, remove header and footer in full screen mode

--- app/articles/[slug]/edit/page.tsx
+++ app/articles/[slug]/edit/page.tsx
@@ -194,7 +194,6 @@ export default function EditArticlePage() {
 
   function clearDraft() {
     localStorage.removeItem(DRAFT_KEY(slug as string));
-    setMessage({ text: "Draft cleared", type: "success" });
   }
 
   async function handleUpdate(e: React.FormEvent) {
@@ -264,7 +263,11 @@ export default function EditArticlePage() {
                 "url(\"data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fillRule='evenodd'%3E%3Cg fill='%239C92AC' fillOpacity='0.1'%3E%3Cpath d='M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0 0v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zM12 34v-4h-2v4H6v2h4v4h2v-4h4v-2h-4zm0 0v-4h-2v4H6v2h4v4h2v-4h4v-2h-4zM36 10v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0 0v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zM12 10v-4h-2v4H6v2h4v4h2v-4h4v-2h-4zm0 0v-4h-2v4H6v2h4v4h2v-4h4v-2h-4z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E\")",
             }}
           ></div>
-          <MinimalHeader />
+          <div className="relative">
+            {" "}
+            {/* Wrapper div with relative positioning */}
+            <MinimalHeader />
+          </div>
         </>
       )}
 
