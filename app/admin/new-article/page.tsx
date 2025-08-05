430597fd486b3334e1029be4f08cc65a1d04d8fe
thaunghtike-share <zwe.zzzin@gmail.com>
2025-08-05 15:06:12 +0630

    author profile saved

--- app/admin/new-article/page.tsx
+++ app/admin/new-article/page.tsx
@@ -498,7 +498,7 @@ export default function NewArticlePage() {
               "url(\"data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fillRule='evenodd'%3E%3Cg fill='%239C92AC' fillOpacity='0.1'%3E%3Cpath d='M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0 0v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zM12 34v-4h-2v4H6v2h4v4h2v-4h4v-2h-4zm0 0v-4h-2v4H6v2h4v4h2v-4h4v-2h-4zM36 10v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0 0v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zM12 10v-4h-2v4H6v2h4v4h2v-4h4v-2h-4zm0 0v-4h-2v4H6v2h4v4h2v-4h4v-2h-4z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E\")",
           }}
         ></div>
-        <div className="sticky top-0 z-50">
+        <div className="top-0 z-50">
           <MinimalHeader />
         </div>
 
@@ -699,7 +699,7 @@ export default function NewArticlePage() {
         }}
       ></div>
       {!fullscreen && (
-        <div className="sticky top-0 z-50">
+        <div className="top-0 z-50">
           <MinimalHeader />
         </div>
       )}
