f6b2648e7eaccfee4b28923bf01eda774473091a
thaunghtike-share <zwe.zzzin@gmail.com>
2025-08-06 15:24:33 +0630

    update home

--- app/admin/new-article/page.tsx
+++ app/admin/new-article/page.tsx
@@ -813,6 +813,12 @@ export default function NewArticlePage() {
                       </div>
                     )}
                     <button
+                      onClick={() => setShowProfileModal(true)}
+                      className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-black transition-colors text-sm font-medium"
+                    >
+                      Edit
+                    </button>
+                    <button
                       onClick={handleLogout}
                       className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition-colors text-sm font-medium"
                     >
